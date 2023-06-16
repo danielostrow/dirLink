3d0adf3e7d8a49cceef8fa2f0b0548af2b7cfe69
Daniel Ostrow <dostro2@students.towson.edu>
2023-06-16 13:27:16 -0400

    Update dirLinkScan.py

--- dirLinkScan.py
+++ dirLinkScan.py
@@ -8,10 +8,12 @@ import subprocess
 
 URL_REGEX_PATTERN = r'(https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+(?:/[\w./?=#&%~-]*))'
 
+
 def browse_directory():
     selected_directory = filedialog.askdirectory()
     if selected_directory:
         scan_directory(selected_directory)
+
 
 def scan_directory(directory):
     for root, dirs, files in os.walk(directory):
@@ -19,13 +21,16 @@ def scan_directory(directory):
             file_path = os.path.join(root, file)
             scan_file(file_path)
 
+
 def scan_file(file_path):
     print(f"Scanning file: {file_path}")
     _, file_extension = os.path.splitext(file_path)
     if file_extension.lower() == '.pdf':
         extract_links_from_pdf(file_path)
+        extract_links_from_panel(file_path)
     else:
         extract_links_from_file(file_path)
+
 
 def extract_links_from_pdf(file_path):
     try:
@@ -39,25 +44,30 @@ def extract_links_from_pdf(file_path):
                 print(f"  - {link}")
             print()
             display_links(file_path, visible_links)
-        
-        # Extract links from the last page of the PDF panel
-        last_page_content = get_last_page_content(file_path)
-        links_last_page = re.findall(URL_REGEX_PATTERN, last_page_content)
-        visible_links_last_page = filter_visible_links(links_last_page)
-        if visible_links_last_page:
-            print(f"Links found in the last page of {file_path}:")
-            for link in visible_links_last_page:
-                print(f"  - {link}")
-            print()
-            display_links(file_path, visible_links_last_page)
-            
     except subprocess.CalledProcessError:
         pass
 
-def get_last_page_content(file_path):
+
+def extract_links_from_panel(file_path):
+    try:
+        panel_content = extract_panel_content(file_path)
+        links_panel = re.findall(URL_REGEX_PATTERN, panel_content)
+        visible_links_panel = filter_visible_links(links_panel)
+        if visible_links_panel:
+            print(f"Links found in the panel object of {file_path}:")
+            for link in visible_links_panel:
+                print(f"  - {link}")
+            print()
+            display_links(file_path, visible_links_panel)
+    except subprocess.CalledProcessError:
+        pass
+
+
+def extract_panel_content(file_path):
     output = subprocess.check_output(['pdftotext', '-q', '-enc', 'UTF-8', '-f', '-1', '-l', '-1', file_path, '-'])
     content = output.decode('utf-8', errors='ignore')
     return content
+
 
 def extract_links_from_file(file_path):
     try:
@@ -74,6 +84,7 @@ def extract_links_from_file(file_path):
     except subprocess.CalledProcessError:
         pass
 
+
 def filter_visible_links(links):
     visible_links = []
     for link in links:
@@ -81,17 +92,20 @@ def filter_visible_links(links):
             visible_links.append(link)
     return visible_links
 
+
 def display_links(file_path, links):
     file_name = os.path.basename(file_path)
     item_id = get_tree_item_id(file_name)
     for link in links:
         tree.insert(item_id, 'end', text=link)
 
+
 def get_tree_item_id(file_name):
     for item in tree.get_children():
         if tree.item(item, 'text') == file_name:
             return item
     return tree.insert('', 'end', text=file_name)
+
 
 def export_to_json():
     links_data = {}
@@ -110,6 +124,7 @@ def export_to_json():
         with open(json_path, 'w') as f:
             json.dump(links_data, f, indent=4)
         print(f"Links exported to {json_path}")
+
 
 # Create a GUI window
 root = tk.Tk()
@@ -131,3 +146,4 @@ export_button.pack(pady=10)
 
 # Start the GUI event loop
 root.mainloop()
+
